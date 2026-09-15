9d275e95153444db28c15cd0c009b0101812b386
Yashwin Sharma <sharma.yashwin01@gmail.com>
2026-09-15 22:47:39 +0530

    Refactor input2.asm to use parentheses for labels

--- input2.asm
+++ input2.asm
@@ -1,45 +1,29 @@
 // Source file hack asembly
-
         @10
-
         D=A
-
         @20
-
         D=D+A
 
-START:
+(START)
         @RESULT
         M=D
-
         @i
         M=0
 
-LOOP:
+(LOOP)
         @i
         M=M+1
-
         @10
         D=M
-
         @i
         D=D-M
-
         @LOOP
         D;JGT
-
         @RESULT
         D=M
-
         @END
         0;JMP
 
-RESULT:
-        0
-
-i:
-        0
-
-END:
+(END)
         @END
         0;JMP
