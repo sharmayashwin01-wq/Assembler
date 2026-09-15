322c0876f0464c6f2de677e8b20c5f49f0731cae
Yashwin Sharma <sharma.yashwin01@gmail.com>
2026-09-15 22:07:39 +0530

    Add files via upload

---
+++ input2.asm
@@ -0,0 +1,45 @@
+// Source file hack asembly
+
+        @10
+
+        D=A
+
+        @20
+
+        D=D+A
+
+START:
+        @RESULT
+        M=D
+
+        @i
+        M=0
+
+LOOP:
+        @i
+        M=M+1
+
+        @10
+        D=M
+
+        @i
+        D=D-M
+
+        @LOOP
+        D;JGT
+
+        @RESULT
+        D=M
+
+        @END
+        0;JMP
+
+RESULT:
+        0
+
+i:
+        0
+
+END:
+        @END
+        0;JMP
